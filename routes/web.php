3dd1ec6ae43a7cfd94509de8c7ce5bc9da03230e
mairo99 <mairo.lokk@gmail.com>
2024-05-19 10:13:05 +0300

    10 peatükk

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
     return view('posts');
 });
-Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-    if (!file_exists($path)) {
-        return redirect('/');
 
+Route::get('posts/{post}', function ($slug) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
+    return redirect('/');
     }
-    $post
-        =
-        file_get_contents($path);
-    return view('post', [
-        'post' => $post
-    ]);
-}) ->where('post', '[A-z_\-]+');
+    $post = cache()->remember ("posts.{$slug}", 1200, fn() => file_get_contents($path));
+    return view('post', ['post' => $post]);
+    })->where('post', '[A-Z_\-]+');
