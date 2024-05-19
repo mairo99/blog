9f53bf80306b912cb178d209542d753ebbd7dfbb
mairo99 <mairo.lokk@gmail.com>
2024-05-19 19:29:41 +0300

    31. peatykk

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,27 @@
 <x-layout>
+    @include('_posts-header')
 
-    @foreach ($posts as $post)
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+
+
+
+
+
+        </div>
+    </main>
+    {{--@foreach ($posts as $post)
         <article>
 
             <a href="/posts/{{$post->slug}}"><h1>
@@ -22,5 +43,6 @@
             </div>
 
         </article>
-    @endforeach
+    @endforeach--}}
+
 </x-layout>
