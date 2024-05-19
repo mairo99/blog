ff456975cd0a53906be4de2d3143e308c3b249f6
mairo99 <mairo.lokk@gmail.com>
2024-05-19 20:35:10 +0300

    32. peatykk

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,47 +2,15 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card />
+        @if ($posts->count())
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
+            <x-posts-grid :posts="$posts"/>
+            @else
+                <p>No posts yet</p>
+            @endif
 
 
-
-
-
-        </div>
     </main>
-    {{--@foreach ($posts as $post)
-        <article>
 
-            <a href="/posts/{{$post->slug}}"><h1>
-                {!!$post->title !!}
-
-            </a>
-            </h1>
-            By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in
-            <a
-
-                href="/categories/{{$post->category->slug}}">{{$post->category->name}}
-
-            </a>
-
-            <div>
-
-                {{$post->excerpt}}
-
-            </div>
-
-        </article>
-    @endforeach--}}
 
 </x-layout>
