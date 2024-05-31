a40181675db90749af481449a640167c72fe4764
mairo99 <mairo.lokk@gmail.com>
2024-05-31 17:55:19 +0300

    42. peatykk

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -16,7 +16,7 @@
     <x-dropdown-item href="/">All</x-dropdown-item>
 
     @foreach ($categories as $category)
-        <x-dropdown-item href="/?categories/{{$category->slug}}"
+        <x-dropdown-item href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category'))}}"
                          :active="isset($currentCategory)&& $currentCategory->is($category)">
             {{ucwords($category->name)}}
 
