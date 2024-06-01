1c9167ff13c374244fc1f68ce4f506a098168afc
mairo99 <mairo.lokk@gmail.com>
2024-06-01 11:50:30 +0300

    46. peatykk

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
 
     }
     }
+
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
 }
