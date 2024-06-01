b412c6034799ad17c1b9b7ac3b0883ffcaf91a58
mairo99 <mairo.lokk@gmail.com>
2024-06-01 14:01:58 +0300

    50. peatykk

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,6 @@ Route::get('authors/{author:username}', function (User $author) {
 
 Route:: get ('register', [RegisterController::class,'create'])->middleware('guest');
 Route:: post ('register', [RegisterController::class,'store'])->middleware('guest');
-Route:: post ('logout', [SessionsController:: class, 'destroy']);
+Route:: get ('login', [SessionsController:: class, 'create'])->middleware('guest');
+Route:: post ('sessions', [SessionsController:: class, 'store'])->middleware('guest');
+Route:: post ('logout', [SessionsController:: class, 'destroy'])->middleware('auth');
