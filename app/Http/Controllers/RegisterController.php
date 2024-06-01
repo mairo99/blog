4872dcbaceecef7fb6fec1689fe9f1dd79a97d61
mairo99 <mairo.lokk@gmail.com>
2024-06-01 12:30:08 +0300

    48. peatykk

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,6 +20,9 @@ class RegisterController extends Controller
             'password' => 'required |min: 7| max: 255' ,
         ]);
         User::create($attributes);
+
+        return redirect ('/' )->with('success', 'Your account has been created.');
+
         return redirect('/');
     }
 
